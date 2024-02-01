b6c28ca4a53e657e7fd09d1464b461b35151863f
Malinkina Natalya <lesnatali1995@gmail.com>
2024-02-01 20:07:02 +0200

    add src

--- src/test/java/ru/netology/statistic/RadioTest.java
+++ src/test/java/ru/netology/statistic/RadioTest.java
@@ -1,9 +1,9 @@
-package ru.netology;
+package ru.netology.statistic;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
+import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
     Radio radio = new Radio();
